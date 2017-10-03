a031c882cfc72a4f1476ad5cb5d9e97a917dabb6
brian51665 <bdavis51892@yahoo.com>
2017-10-03 19:05:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 function takeANumber(array, name) {
   var linePosition = array.length + 1;
   linePosition.push(name);
-    return "Welcome, " + `${name}` + ". You are number " + (emptyArray.length) + " in line.";
+    return "Welcome, " + name + ". You are number " + linePosition + " in line.";
   }
