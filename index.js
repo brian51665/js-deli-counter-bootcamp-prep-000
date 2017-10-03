17a34683fd3210c0fbfa9ebb4f9b1fc57afbebf7
brian51665 <bdavis51892@yahoo.com>
2017-10-03 19:44:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,6 @@ function currentLine(array) {
  if (array.length === 0){
    return "The line is currently empty."
  } else {
-   var i = 0
-   for (i = 0; i < array.length + 1; i++)
-   return "The line is currently : " + (i + 1) + ". " + array[i];
-   i++;
+      return "The line is currently : " + 
  }
 }
