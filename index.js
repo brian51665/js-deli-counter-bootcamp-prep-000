088a2273712b57b084971e26bc5de263d5be0e42
brian51665 <bdavis51892@yahoo.com>
2017-10-03 19:05:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function takeANumber(Array, name) {
+  var emptyArray = [];
+  emptyArray.push(`${name});
+    return "Welcome, " + `${name}` + ". You are number " + (emptyArray.length) + " in line.";
+  }
