56a556f3a29367d81fe161a97178f4160f0d3b93
brian51665 <bdavis51892@yahoo.com>
2017-10-03 19:39:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(array) {
  } else {
    var i = 0
    for (i = 0; i < array.length + 1; i++)
-   return "The line is currently : " + (array.length + 1) + ". " + array[i];
+   return "The line is currently : " + (array[i] + 1) + ". ";
    i++;
  }
 }
